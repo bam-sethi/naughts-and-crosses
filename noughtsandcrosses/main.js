0f2172ff98e0b349a31d1d91a7baca0e586c1bf4
Ambar Sethi <ambarrrsethi@gmail.com>
2015-08-16 19:24:14 +0100

    Fancy fonts

--- noughtsandcrosses/main.js
+++ noughtsandcrosses/main.js
@@ -5,13 +5,10 @@ square.addEventListener("click", function(){
   if (player === 'x'){
   player ='o';
   this.innerHTML = player;
-  console.log("hi");
-} else {
+  } else {
   player = 'x';
   this.innerHTML = player;
-  console.log('this X');
-}
-console.log('this ?')
+  }
 });
 
 var square2 = document.getElementById("a2");
@@ -19,107 +16,112 @@ square2.addEventListener("click", function(){
   if (player === 'x'){
   player ='o';
   square2.innerHTML = player;
-  console.log("hi");
-} else {
-  player = 'x';
-  square2.innerHTML = player;
-  console.log('this X');
-}
-console.log('this ?')
+
+  } else {
+    player = 'x';
+    square2.innerHTML = player;
+
+  }
 });
 
 var square3 = document.getElementById("a3");
 square3.addEventListener("click", function(){
-  if (player === 'x'){
-  player ='o';
-  square3.innerHTML = player;
-} else {
-  player = 'x';
-  square3.innerHTML = player;
-}
-//row 2
+    if (player === 'x'){
+    player ='o';
+    square3.innerHTML = player;
+  } else {
+    player = 'x';
+    square3.innerHTML = player;
+  }
 });
+//row 2
 var square4 = document.getElementById("b1");
 square4.addEventListener("click", function(){
   if (player === 'x'){
   player ='o';
   square4.innerHTML = player;
-} else {
-  player = 'x';
-  square4.innerHTML = player;
-}
-
+  } else {
+    player = 'x';
+    square4.innerHTML = player;
+  }
 });
 var square5 = document.getElementById("b2");
 square5.addEventListener("click", function(){
   if (player === 'x'){
   player ='o';
   square5.innerHTML = player;
-} else {
-  player = 'x';
-  square5.innerHTML = player;
-}
-
+  } else {
+    player = 'x';
+    square5.innerHTML = player;
+  }
 });
 var square6 = document.getElementById("b3");
 square6.addEventListener("click", function(){
   if (player === 'x'){
   player ='o';
   square6.innerHTML = player;
-} else {
-  player = 'x';
-  square6.innerHTML = player;
-}
-//row 3
+  } else {
+    player = 'x';
+    square6.innerHTML = player;
+  }
 });
+//row 3
 var square7 = document.getElementById("c1");
 square7.addEventListener("click", function(){
   if (player === 'x'){
   player ='o';
   square7.innerHTML = player;
-} else {
-  player = 'x';
-  square7.innerHTML = player;
-}
-
+  } else {
+    player = 'x';
+    square7.innerHTML = player;
+  }
 });
 var square8 = document.getElementById("c2");
 square8.addEventListener("click", function(){
   if (player === 'x'){
   player ='o';
   square8.innerHTML = player;
-} else {
-  player = 'x';
-  square8.innerHTML = player;
-}
-
+  } else {
+    player = 'x';
+    square8.innerHTML = player;
+  }
 });
 var square9 = document.getElementById("c3");
 square9.addEventListener("click", function(){
   if (player === 'x'){
   player ='o';
   square9.innerHTML = player;
-} else {
-  player = 'x';
-  square9.innerHTML = player;
-}
-
+  } else {
+    player = 'x';
+    square9.innerHTML = player;
+  }
 });
 
 
-var playAgain = document.getElementById("play-more");
-playAgain.addEventListener("click", function(){
-  //when clicked reset the board
-  var board = document.getElementById("a1");
-  board.innerHTML = " ";
 
-})
+  
 
-///
 
-///////// 
-///how to tell you when there is a winner
-//
+
+
+
+// //put this in an if statement or something
+// var playAgain = document.getElementsByClassName("play-again");
+// var board = document.getElementsByClassName("grid");
+// for (var i = 0; i < board.length; i ++){
+//   this.addEventListener("click", function(){
+//   board.innerHTML = " ";
+
+// });
+// }
+
+var playAgainButton = document.getElementsByClassName("play-again")
+addEventListener("click", function(){
+  var board = document.getElementsByClassName("grid");
+  board.innerHTML = "";  // Replaces body content with an empty string.
+});
+
+
 
 
 
