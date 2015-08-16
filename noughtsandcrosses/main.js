9934b40c5548d6831e9e5630e740490c13672166
Ambar Sethi <ambarrrsethi@gmail.com>
2015-08-16 15:54:33 +0100

    basic board functioning

--- noughtsandcrosses/main.js
+++ noughtsandcrosses/main.js
@@ -1,26 +1,111 @@
 var player;
-
-var box = document.getElementById("a1")
-this.addEventListener("click", function(){
+//row1
+var square = document.getElementById("a1");
+square.addEventListener("click", function(){
   if (player === 'x'){
   player ='o';
-  this.innerHTML = "o";
+  this.innerHTML = player;
   console.log("hi");
 } else {
   player = 'x';
-  this.innerHTML = 'x';
+  this.innerHTML = player;
   console.log('this X');
 }
 console.log('this ?')
 });
 
+var square2 = document.getElementById("a2");
+square2.addEventListener("click", function(){
+  if (player === 'x'){
+  player ='o';
+  square2.innerHTML = player;
+  console.log("hi");
+} else {
+  player = 'x';
+  square2.innerHTML = player;
+  console.log('this X');
+}
+console.log('this ?')
+});
 
-// var box2 = document.addEventListener("click", function(){
-//   var square = document.getElementById("a2");
-//   player === "X" ? "O" :"X";
-//   square.innerHTML = player;
-    
-// });
+var square3 = document.getElementById("a3");
+square3.addEventListener("click", function(){
+  if (player === 'x'){
+  player ='o';
+  square3.innerHTML = player;
+} else {
+  player = 'x';
+  square3.innerHTML = player;
+}
+//row 2
+});
+var square4 = document.getElementById("b1");
+square4.addEventListener("click", function(){
+  if (player === 'x'){
+  player ='o';
+  square4.innerHTML = player;
+} else {
+  player = 'x';
+  square4.innerHTML = player;
+}
+
+});
+var square5 = document.getElementById("b2");
+square5.addEventListener("click", function(){
+  if (player === 'x'){
+  player ='o';
+  square5.innerHTML = player;
+} else {
+  player = 'x';
+  square5.innerHTML = player;
+}
+
+});
+var square6 = document.getElementById("b3");
+square6.addEventListener("click", function(){
+  if (player === 'x'){
+  player ='o';
+  square6.innerHTML = player;
+} else {
+  player = 'x';
+  square6.innerHTML = player;
+}
+//row 3
+});
+var square7 = document.getElementById("c1");
+square7.addEventListener("click", function(){
+  if (player === 'x'){
+  player ='o';
+  square7.innerHTML = player;
+} else {
+  player = 'x';
+  square7.innerHTML = player;
+}
+
+});
+var square8 = document.getElementById("c2");
+square8.addEventListener("click", function(){
+  if (player === 'x'){
+  player ='o';
+  square8.innerHTML = player;
+} else {
+  player = 'x';
+  square8.innerHTML = player;
+}
+
+});
+var square9 = document.getElementById("c3");
+square9.addEventListener("click", function(){
+  if (player === 'x'){
+  player ='o';
+  square9.innerHTML = player;
+} else {
+  player = 'x';
+  square9.innerHTML = player;
+}
+
+});
+
 
 
 
